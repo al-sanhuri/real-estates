ca0618dd2a0ccf02286e93785d1d3235d80064eb
al-sanhuri <103034937+al-sanhuri@users.noreply.github.com>
2022-06-15 13:42:43 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let navLinks = document.querySelector('nav .nav-links');
+function showMenue() {
+    navLinks.style.display = 'block';
+}
+function hideMenue() {
+    navLinks.style.display = 'none';
+}
